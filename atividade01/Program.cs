9e0c6b84f5660529c063d174bc09e16914903be5
brunoleonards <114749297+brunoleonards@users.noreply.github.com>
2022-10-21 09:36:16 -0300

    Add files via upload

---
+++ atividade01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 100; i >= 1; i = i - 1) 
+            {
+                Console.WriteLine("Esse é" + i + "ª");
+            }
+            for ( int L = 1; L <= 100; L = L + 1)
+            {
+                Console.WriteLine("Serie é "+ L +"º");
+            }
+            
+        }
+       
+        
+    }
+}
